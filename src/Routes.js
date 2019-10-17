02959c2f2bae382cebe07caf4dffcdd29d808ff5
Michel Manarin <MichelManarin@gmail.com>
2019-10-17 13:29:17 -0300

    Ajuste

--- src/Routes.js
+++ src/Routes.js
@@ -1,18 +1,16 @@
 import React from "react";
 
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { HashRouter, Route } from 'react-router-dom';
 
 import App from './App';
 import NotFoundPage from './components/NotFoundPage/NotFoundPage';
 import Historic from './views/Historic/Historic'
 
 const Routes = () => (
-    <BrowserRouter basename='/weather-app/'>
-      <Switch>
-      <Route exact path='/' component={() => <App/>} />
-      <Route path='*' component={() => <Historic/>} />
-      </Switch>
-    </BrowserRouter>
+    <HashRouter>
+      <Route path='/weather-app' exact={true} component={() => <App/>} />
+      <Route path='/weather-app/history'  component={() => <Historic/>} />
+    </HashRouter>
   );
 
 export default Routes;
